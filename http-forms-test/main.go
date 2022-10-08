1611c82be7d9014a8a4b1089c38373e066c3fc7a
Antonio Leonti <antoniovleonti@icloud.com>
2022-10-08 17:25:40 -0400

    finish long polling test

--- http-forms-test/main.go
+++ http-forms-test/main.go
@@ -23,18 +23,36 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
       word = r.FormValue("prefix") + word + r.FormValue("suffix")
       fmt.Printf("new word: '%s'\n", word)
       fmt.Fprint(w, word)
+      // broadcast new word to all long poll listeners
+      for _, c := range longPollChannels {
+        c <- word
+      }
+      longPollChannels = make([]chan string, 0) // clear
+    default:
+      http.Error(w, "", http.StatusMethodNotAllowed)
   }
 }
 
-func longPollHandler(w http.ResponseWriter, r *http.Request) {
+func wordHandler(w http.ResponseWriter, r *http.Request) {
+  switch r.Method {
+    case http.MethodGet:
+      fmt.Fprint(w, word)
+    default:
+      http.Error(w, "", http.StatusMethodNotAllowed)
+  }
+}
+
+func nextWordHandler(w http.ResponseWriter, r *http.Request) {
   myChan := make(chan string)
   longPollChannels = append(longPollChannels, myChan)
   fmt.Fprint(w, <-myChan)
 }
 
 func main() {
+  longPollChannels = make([]chan string, 0)
   http.HandleFunc("/", rootHandler) // setting router rule
-  http.HandleFunc("/long-poll", longPollHandler) // setting router rule
+  http.HandleFunc("/word", wordHandler)
+  http.HandleFunc("/long-poll", nextWordHandler)
 
   err := http.ListenAndServe(":9090", nil) // setting listening port
   if err != nil {
