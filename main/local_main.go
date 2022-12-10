85a7d73e0af691ae0840368146dab27ca3389c13
graissant <graysonkelly.1@gmail.com>
2022-12-10 18:33:04 -0500

    local_main.go for development

---
+++ main/local_main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"sgserver"
+)
+
+func main() {
+	rooms := make(map[string]*sgserver.RoomWrapper)
+	server := sgserver.NewSuperghostServer(rooms)
+	http.ListenAndServe(":9090",
+		server.Router)
+}
